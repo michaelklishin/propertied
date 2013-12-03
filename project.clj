88885edabbd522df66188ccf5f27970d45979801
Michael Klishin <michael@clojurewerkz.org>
2013-12-03 22:24:44 +0400

    Back to dev version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/propertied "1.1.0"
+(defproject clojurewerkz/propertied "1.2.0-SNAPSHOT"
   :description "Tiny Clojure library for working with property lists (java.util.Properties)"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clojurewerkz/support "0.17.0"]]
